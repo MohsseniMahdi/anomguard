b86ac9f1702da8cb8d450c566304f7a04dbdd059
Milan Martonfi <milan.martonfi@gmail.com>
2025-03-13 16:57:57 +0100

    Updated Mahdi_V1.5_M.ipynb, data.py, and fast.py

--- api/fast.py
+++ api/fast.py
@@ -1,12 +1,9 @@
-# TODO: Import your package, replace this by explicit imports of what you need
-from anomguard.interface.main_local import predict
-
+from anomguard.ml_logic import model, preprocessing, data
 from fastapi import FastAPI, File, UploadFile, HTTPException
 import pandas as pd
-import os
+import io
+from pathlib import Path
 from fastapi.middleware.cors import CORSMiddleware
-# from taxifare.ml_logic.registry import  load_model
-# from taxifare.ml_logic.preprocessor import preprocess_features
 
 app = FastAPI()
 
@@ -18,17 +15,32 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
-# Endpoint for https://your-domain.com/
 @app.get("/")
 def root():
     return {
         'message': "Hi, The API is running!"
     }
 
-# Endpoint for https://your-domain.com/predict?input_one=154&input_two=199
-@app.get("/predict")
-def get_predict():
+@app.post("/predict")
+async def predict_fraud(file: UploadFile = File(...)):
+    try:
+        contents = await file.read()
+        df = pd.read_csv(io.BytesIO(contents))
 
+        # Adjust data extraction
+        X = df.drop('Class', axis=1)
+        y = df['Class']
 
+        # Preprocess the data
+        X_train, X_test, y_train_smote = preprocessing.preprocessing_smote(X,X,y)
 
-    return { predict():'greet'}
+        # Make predictions
+        predictions = model.predict(X_test) #X_test needs to be preprocessed.
+
+        # Convert predictions to a list of dictionaries
+        results = [{"prediction": int(p)} for p in predictions]
+
+        return {"predictions": results}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
