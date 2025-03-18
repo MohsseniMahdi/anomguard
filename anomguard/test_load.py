69f37bddecedd03b0f84775a187ff3adb0f011ba
Milan Martonfi <milan.martonfi@gmail.com>
2025-03-18 09:33:40 +0100

    V2 features updated

--- anomguard/test_load.py
+++ anomguard/test_load.py
@@ -1,13 +1,18 @@
 from anomguard.ml_logic.registry import load_model
+from anomguard.ml_logic.preprocessing import preprocessing_V2_features
 import pandas as pd
 def model_test():
     model = load_model()
     X_pred = pd.read_csv('X_test.csv')
+    X_pred = X_pred.drop(columns = 'Unnamed: 0')
     y_pred  = model.predict(X_pred)
     print(y_pred)
     return y_pred
 
-
+def preproccesingv2_check():
+    X_pred = pd.read_csv('X_test.csv')
+    X_transformed = preprocessing_V2_features(X_pred)
+    return X_transformed.shape
 
 if __name__ == "__main__":
-    model_test ()
+    preproccesingv2_check()
